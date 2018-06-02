104074049a6f5fb57f30c276e1c1f7b0246ec0d0
Mario <marioschantel@gmail.com>
2018-06-02 16:29:42 +0200

    CodeReview 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AngularFireModule} from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './content/content.component';
+import { HeroComponent } from './hero/hero.component';
+
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB9_7N32G1dVc6jTuX0Y0KjdNAUzOjUV3w',
+  authDomain: 'datingappdata.firebaseapp.com',
+  databaseURL: 'https://datingappdata.firebaseio.com',
+  projectId: 'datingappdata',
+  storageBucket: 'datingappdata.appspot.com',
+  messagingSenderId: '76997266933'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContentComponent,
+    HeroComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    FormsModule
+   ],
   providers: [],
   bootstrap: [AppComponent]
-})
+ })
+
 export class AppModule { }
---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,16 @@
+
+import { Component, OnInit } from '@angular/core';
+@Component({
+selector: 'hero',
+templateUrl: './hero.component.html',
+styleUrls: ['./hero.component.css']
+})
+export class HeroComponent implements OnInit {
+private heroHeading: string;
+private heroText: string;
+private heroBtnText: string;
+private heroBtnUrl: string;
+constructor() { }
+ngOnInit() {
+}
+}
